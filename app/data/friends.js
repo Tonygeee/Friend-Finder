b185477b2de3c4eaa506b975c5f91f1fae6144c1
Tony Gonzalez <Tony@AntoniosMacBook.localdomain>
2018-04-25 22:55:06 -0700

    bunch of routing fixes

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Tony",
+		photo: "https://ei.marketwatch.com/Multimedia/2017/01/19/Photos/ZH/MW-FD964_jackma_20170119044803_ZH.jpg?uuid=5f13cb7e-de2c-11e6-9520-001cc448aede",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Danielle",
+		photo: "http://blog.lumoid.com/wp-content/uploads/senior-woman-eating-red-blue-popsicle-9598360.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Robbie",
+		photo: "http://ap_images.s3.amazonaws.com/article_photos/000/002/886/cb355a67-e3a3-4107-94a8-983f5b1b605c_original.",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Alexys",
+		photo: "https://thumb1.shutterstock.com/mosaic_250/2967241/666258808/stock-photo-portrait-of-young-beautiful-cute-cheerful-girl-smiling-looking-at-camera-over-white-background-666258808.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "David",
+		photo: "https://st.depositphotos.com/1211672/1670/i/950/depositphotos_16702965-stock-photo-man-with-down-syndrome-soldier.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
